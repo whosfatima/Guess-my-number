4fbede398b5ce4afbcf20359fe2318b9dcc20a19
fatima <fatimaisherewithyou@gmail.com>
2026-06-28 23:03:04 +0330

    Logic stuff

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const ModalBox = document.getElementById('modalBox')
 const OPenModal = document.getElementById('OPenModal')
 const StartRansom = document.getElementById('StartRansom')
 const paraVal = document.querySelector('.value')
+const warning = document.createElement('p')
 
+ModalBox.appendChild(warning)
 OPenModal.addEventListener('click', () => {
     ModalBox.classList.remove('none')
     ModalBox.classList.add('modalBox')
@@ -16,32 +18,44 @@ SaveValue.addEventListener('click', () => {
     let minNumberVal = +minNumber.value
     let maxNumberVal = +maxNumber.value
 
-    paraVal.textContent = `numbers are between ${minNumberVal} - ${maxNumberVal}`
-    
- 
-    ModalBox.classList.remove('modalBox')
-    ModalBox.classList.add('none')
+    if (minNumberVal >= maxNumberVal) {
+        // alert('max must be bigger than min...')
+        console.log('bloom1');
+        warning.classList.remove('none')
+        warning.classList.add('warning')
+        warning.append('MAX must be more than MIN!!!')
+     
+    } else if(maxNumberVal > minNumberVal) {
+        console.log('bloom2');
+        paraVal.textContent = `numbers are between ${minNumberVal} - ${maxNumberVal}`
+        ModalBox.classList.remove('modalBox')
+        ModalBox.classList.add('none')
+
+        warning.classList.remove('warning')
+        warning.classList.add('none')
+    }
+
+
     // minNumber.value=
     // maxNumber.value=
 
-  
+
 })
 StartRansom.addEventListener('click', () => {
-    let maxInput= +maxNumber.value 
-    let minInput= +minNumber.value 
+    let maxInput = +maxNumber.value
+    let minInput = +minNumber.value
 
     // let minInputNumber= +minInput
-    console.log('MIN=' +minInput);
-    console.log('MAX= '+maxInput);
+    console.log('MIN=' + minInput);
+    console.log('MAX= ' + maxInput);
     // console.log(minInputNumber);
-    
-randomNum( minInput, maxInput)
+
+    let randomNumber= randomNum(minInput, maxInput)
 })
 
 function randomNum(minNumber, maxNumber) {
-    let random = Math.floor(Math.random() *(maxNumber - minNumber)) + minNumber 
+    let random = Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber
     console.log(random);
-
+    return random
 }
-// randomNum(maxNumbervali)
-// console.log( typeof randomNum() );
+
