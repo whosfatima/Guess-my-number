b2a56e8cdf932746b762731d4cf579f571c1f377
fatima <fatimaisherewithyou@gmail.com>
2026-06-29 00:35:03 +0330

    Woriking on logic 24/7

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ const SaveValue = document.getElementById('SaveValue')
 const ModalBox = document.getElementById('modalBox')
 const OPenModal = document.getElementById('OPenModal')
 const StartRansom = document.getElementById('StartRansom')
+const guessPart = document.getElementById('guessPart')
 const paraVal = document.querySelector('.value')
 const warning = document.createElement('p')
-
 ModalBox.appendChild(warning)
+
+const result= document.querySelector('.result')
+
 OPenModal.addEventListener('click', () => {
     ModalBox.classList.remove('none')
     ModalBox.classList.add('modalBox')
@@ -20,19 +23,21 @@ SaveValue.addEventListener('click', () => {
 
     if (minNumberVal >= maxNumberVal) {
         // alert('max must be bigger than min...')
-        console.log('bloom1');
+        warning.innerHTML=''
+        // console.log('bloom1');
         warning.classList.remove('none')
         warning.classList.add('warning')
         warning.append('MAX must be more than MIN!!!')
      
     } else if(maxNumberVal > minNumberVal) {
-        console.log('bloom2');
+        // console.log('bloom2');
         paraVal.textContent = `numbers are between ${minNumberVal} - ${maxNumberVal}`
         ModalBox.classList.remove('modalBox')
         ModalBox.classList.add('none')
 
         warning.classList.remove('warning')
         warning.classList.add('none')
+        
     }
 
 
@@ -42,15 +47,24 @@ SaveValue.addEventListener('click', () => {
 
 })
 StartRansom.addEventListener('click', () => {
+    // result.innerHTML=' '
     let maxInput = +maxNumber.value
     let minInput = +minNumber.value
-
+let GuessNumb= +guessPart.value
     // let minInputNumber= +minInput
-    console.log('MIN=' + minInput);
-    console.log('MAX= ' + maxInput);
+    // console.log('MIN=' + minInput);
+    // console.log('MAX= ' + maxInput);
     // console.log(minInputNumber);
 
     let randomNumber= randomNum(minInput, maxInput)
+    let notran= randomNumber
+    console.log(randomNumber);
+    console.log(notran);
+    
+    
+    // result.append(randomNumber)
+    if(GuessNumb === randomNumber) console.log('so>');
+    
 })
 
 function randomNum(minNumber, maxNumber) {
