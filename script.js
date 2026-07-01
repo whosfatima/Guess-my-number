a769f88dcb0260ff7ed2f0681059d8eff4058bd8
fatima <fatimaisherewithyou@gmail.com>
2026-07-01 13:04:05 +0330

    On Js

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 const minNumber = document.getElementById('minNumber')
 const maxNumber = document.getElementById('maxNumber')
-
 const SaveValue = document.getElementById('SaveValue')
 const ModalBox = document.getElementById('modalBox')
 const OPenModal = document.getElementById('OPenModal')
 const StartRansom = document.getElementById('StartRansom')
 const guessPart = document.getElementById('guessPart')
+
 const paraVal = document.querySelector('.value')
+const group = document.querySelector('.group')
+const result = document.querySelector('.result')
+
 const warning = document.createElement('p')
+// const step= document.createElement('p')
+// const score= document.createElement('p')
+const SpanStep = document.createElement('span')
+
+// step.append('Step: ')
+// SpanStep.append('20')
 ModalBox.appendChild(warning)
-let avRandom=0
-const result= document.querySelector('.result')
+// group.appendChild(step, score)
+// step.appendChild(SpanStep)
+
+let avRandom = 0
+let guessStep = 20
+
+
+
+
+
 
 OPenModal.addEventListener('click', () => {
     ModalBox.classList.remove('none')
@@ -22,57 +39,46 @@ SaveValue.addEventListener('click', () => {
     let maxNumberVal = +maxNumber.value
 
     if (minNumberVal >= maxNumberVal) {
-        // alert('max must be bigger than min...')
-        warning.innerHTML=''
-        // console.log('bloom1');
+        warning.innerHTML = ''
         warning.classList.remove('none')
         warning.classList.add('warning')
         warning.append('MAX must be more than MIN!!!')
-     
-    } else if(maxNumberVal > minNumberVal) {
-        // console.log('bloom2');
+
+    } else if (maxNumberVal > minNumberVal) {
         paraVal.textContent = `numbers are between ${minNumberVal} - ${maxNumberVal}`
         ModalBox.classList.remove('modalBox')
         ModalBox.classList.add('none')
 
         warning.classList.remove('warning')
         warning.classList.add('none')
-        avRandom= randomNum(minNumberVal, maxNumberVal)
-        
+        avRandom = randomNum(minNumberVal, maxNumberVal)
+
     }
 
-console.log(avRandom);
-
-    // minNumber.value=
-    // maxNumber.value=
+    console.log(avRandom);
 
 
 })
 StartRansom.addEventListener('click', () => {
-    // result.innerHTML=' '
+    result.innerHTML = ''
     let maxInput = +maxNumber.value
     let minInput = +minNumber.value
-let GuessNumb= +guessPart.value
-if(avRandom=== GuessNumb) alert('same')
-    // let minInputNumber= +minInput
-    // console.log('MIN=' + minInput);
-    // console.log('MAX= ' + maxInput);
-    // console.log(minInputNumber);
-
-    // let randomNumber= randomNum(minInput, maxInput)
-    // let notran= randomNumber
-    // console.log(randomNumber);
-    // console.log(notran);
-    
-    
-    // // result.append(randomNumber)
-    // if(GuessNumb === randomNumber) console.log('so>');
-    
+    let GuessNumb = +guessPart.value
+    if (avRandom === GuessNumb) {
+        result.append(avRandom)
+    } else {
+        let guessStepMines = guessStep--
+        console.log(guessStep);
+        group.innerHTML = `
+      <p id="step"><span>Step:</span>${guessStep}</p>
+            <p id="score"><span>Score:</span></p>
+`
+    }
 })
+
 
 function randomNum(minNumber, maxNumber) {
     let random = Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber
     console.log(random);
     return random
 }
-
